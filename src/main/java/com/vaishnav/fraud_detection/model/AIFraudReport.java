11fc1fb294413d92b423e64d61d31641356b3016
vaishnavbhosale <vaishnavbharatbhosale@gmail.com>
2026-06-23 16:53:34 +0530

    Added the aifraudreport model class

--- src/main/java/com/vaishnav/fraud_detection/model/AIFraudReport.java
+++ src/main/java/com/vaishnav/fraud_detection/model/AIFraudReport.java
@@ -1,4 +1,23 @@
 package com.vaishnav.fraud_detection.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AIFraudReport {
+
+    private Integer riskScore;
+
+    private String fraudCategory;
+
+    private String explanation;
+
+    private String recommendation;
+
+
+
 }
